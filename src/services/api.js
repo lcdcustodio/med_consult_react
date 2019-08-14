e9d7ded4b6e030fd65f692604fb2680a58cfa3c3
oberdan <oberdan@alis-sol.com.br>
2019-08-14 18:34:11 -0300

    ajustando api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,21 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://appmedconsultor.rededor.com.br/medico-consultor-api';
+axios.defaults.baseURL = 'http://appmedconsultor-hmg.rededor.com.br/medico-consultor-api';
+
+axios.interceptors.request.use(function (config) {
+	console.log(config);
+	return config;
+}, function (error) {
+	console.log(error);
+	return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response) {
+	console.log(response);
+	return response;
+}, function (error) {
+	console.log(error);
+	return Promise.reject(error);
+});
 
 export default axios;
