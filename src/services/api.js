085ddb2fd977e430764eb178f043c038f3721bc1
oberdan <oberdan@alis-sol.com.br>
2019-08-14 17:32:53 -0300

    Atualizando branch release com namespace e nome correto

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://appmedconsultor-hmg.rededor.com.br/medico-consultor-api';
+axios.defaults.baseURL = 'https://appmedconsultor.rededor.com.br/medico-consultor-api';
 
 axios.interceptors.request.use(function (config) {
 	console.log(config);
