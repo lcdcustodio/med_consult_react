ea68a52c7d2855ad802c3ccc27ce52c04df975b9
oberdan <oberdan@alis-sol.com.br>
2019-11-21 16:20:27 -0300

    adicionando arquivo build

--- build.sh
+++ build.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
+PACKAGE_VERSION=$(node -p -e "require('./app.json').version")
+PACKAGE_NAME=$(node -p -e "require('./app.json').name")
+
+if [ "$PACKAGE_VERSION" =  'undefined' ]; then
+    echo "Informe o número da versão"; exit 1;
+fi
+
+if [ "$PACKAGE_NAME" =  'undefined' ]; then
+    echo "Informe o número do app"; exit 2;
+fi
+
 cd android && ./gradlew assembleRelease
+
+export APP_NAME="$PACKAGE_NAME-$PACKAGE_VERSION";
+
+find . -type f -name '*.ipa' -exec bash -c 'x="{}"; mv "$x" "$APP_NAME.ipa";' \;
+find . -type f -name '*.apk' -exec bash -c 'x="{}"; mv "$x" "$APP_NAME.apk";' \;
+
+
+ls $APP_NAME.*;
+
+cd ios
+
+#xcodebuild -scheme medicoconsultor archive -archivePath ./medicoconsultor.xcarchive
+
+#xcodebuild -exportArchive -archivePath medicoconsultor.xcarchive -exportPath medicoconsultor.ipa -exportOptionsPlist medicoconsultor/Info.plist -exportProvisioningProfile "Medico_Consultor_RDSL"
